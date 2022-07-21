42132041da8debba862c87a36a4a7a30eddd4b51
Xin Zhen (Ray) Goh <xinzhengoh@gmail.com>
2022-07-20 17:11:01 -0700

    Basic functionality to integrate with AWS Cognito

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  cognito: {
+    userPoolId: 'us-west-1_iA9VCrimy',
+    userPoolWebClientId: 'plq34jt7o2lekqgukfbj4mj75',
+  }
 };
 
 /*
